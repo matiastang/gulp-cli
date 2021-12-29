7c1e97dbe667532d829239e3e848849470ce2bf3
唐道勇 <matias@tangdaoongdeMBP.lan>
2021-12-29 16:49:28 +0800

    feat: - 添加ts支持

---
+++ gulpfile.ts/config.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: tangdaoyong
+ * @Date: 2021-05-07 15:13:57
+ * @LastEditors: tangdaoyong
+ * @LastEditTime: 2021-05-07 15:14:15
+ * @Description: gulp config
+ */
+const level = 0
+export {
+    level,
+}
---
+++ gulpfile.ts/index.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: matiastang
+ * @Date: 2021-12-24 14:50:33
+ * @LastEditors: matiastang
+ * @LastEditTime: 2021-12-29 16:48:44
+ * @FilePath: /gulp-cli/gulpfile.ts/index.ts
+ * @Description: gulpfile.js
+ */
+// 导出所有 tasks
+export * from './tasks/git'
+export * from './tasks/npm'
+export * from './tasks/scripts'
+export * from './tasks/version'
